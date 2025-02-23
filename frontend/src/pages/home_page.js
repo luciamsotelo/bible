f819fa3a884122c29064ac2339a20a878e17cc50
Carlos Hernandez <chernandezmiranda@gmail.com>
2025-02-23 15:24:47 -0800

    added .module to home_page to make the hero image responsive.

--- frontend/src/pages/home_page.js
+++ frontend/src/pages/home_page.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import Header from '../components/header'
-import Footer from '../components/footer'
-import biblePicture from '../images/bible.jpg'
-import MemoryVerse from '../components/memory_verse'
-import { Image } from 'react-bootstrap'
+import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import biblePicture from "../images/bible.jpg";
+import MemoryVerse from "../components/memory_verse";
+import { Image } from "react-bootstrap";
+import styles from "../styles/home_page.module.css";
 
-const home_page = () => {
+const HomePage = () => {
   return (
     <div>
-        <Header />
-        <MemoryVerse/>
-        <div className="text-center" >
-          <Image className='rounded' src={biblePicture} alt="Bible" fluid style={{height: "50vh", marginBottom:'18vh'}} />
-        </div>
-        
-        <Footer />
+      <Header />
+      <MemoryVerse />
+      <div className="text-center">
+        <Image
+          className={`rounded ${styles.responsiveImage}`}
+          src={biblePicture}
+          alt="Bible"
+        />
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default home_page
-
+export default HomePage;
