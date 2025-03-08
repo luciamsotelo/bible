ce8c9f6e3e3e3feecd5be8fe38a782504a92d79e
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2025-03-08 10:48:31 -0800

    css styling for map adventure

--- frontend/src/pages/AdventureMap_page.js
+++ frontend/src/pages/AdventureMap_page.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Header from '../components/header'
 import Map from '../components/adventure_map'
-import Footer from '../components/footer'
 
 const AdventureMap_page = () => {
   return (
     <div>
         <Header /> 
         <Map />
-        <Footer />
+  
     </div>
   )
 }
