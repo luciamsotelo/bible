68597fa1a6783d6f7ce7a5c29c76e0daf5553f93
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2025-04-16 09:06:09 -0700

    spelling error fix

--- frontend/src/components/puzzleJoshpAndCoat.js
+++ frontend/src/components/puzzleJoshpAndCoat.js
@@ -75,7 +75,7 @@ const Puzzle = () => {
                 key={index}
                 className={styles.josephPuzzlePiece}
                 style={{
-                  backgroundImage: "url('/images/jospephCoatColors.png')",
+                  backgroundImage: "url('/images/josephCoatColors.png')",
                   backgroundPosition: `${(piece % GRID_SIZE) * (100 / (GRID_SIZE - 1))}% ${
                     Math.floor(piece / GRID_SIZE) * (100 / (GRID_SIZE - 1))
                   }%`,
